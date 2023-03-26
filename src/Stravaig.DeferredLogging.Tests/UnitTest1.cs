9c5fc6d6546c8101c4203b9f65ec67d866f9a3c4
Colin Mackay <github@colinmackay.scot>
2023-03-26 17:58:14 +0100

    Add projects

---
+++ src/Stravaig.DeferredLogging.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Stravaig.DeferredLogging.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
